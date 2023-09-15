726770a52f062e7ea2c53954b3f34e5370737091
sean <arthurxxy@gmail.com>
2023-09-15 13:00:29 +0800

    Initial Commit

---
+++ Calibre/CalibreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalibreApp.swift
+//  Calibre
+//
+//  Created by sean on 2023/9/15.
+//
+
+import SwiftUI
+
+@main
+struct CalibreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
